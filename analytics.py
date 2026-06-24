dec945f5381fe83ad4a69014d598024944f204fc
Adrien P <adrienponce7@gmail.com>
2026-06-24 14:57:48 -0400

    Finished most of the routes for now.

--- analytics.py
+++ analytics.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import math
-from sqlmodel import and_, Session, or_, select
+from sqlmodel import and_, or_, Session, select
 
 from models import Match
 
@@ -17,10 +17,15 @@ class Analytics:
     competition_id: int
     season_id: int 
 
-    def get_league_goal_averages(self) -> tuple[float, float]:
+    def league_goal_averages(self) -> tuple[float, float]:
         """
         Find and return the home and away goal averages for a particular season in the 
         given competition.
+        
+        Returns:
+            tuple[float, float]: The current competition season total:
+                - Home goal averages
+                - Away goal averages
         """
         filter = select(Match).where(
             and_(
@@ -42,12 +47,24 @@ class Analytics:
         
         return h_goal_avg, a_goal_avg
 
-    def get_team_strengths(self, team_id: int) -> tuple[float, float, float, float]:
-        """ 
-        Find and return the attacking and defensive strengths for the given home and 
-        away teams.
+    def team_strengths(self, team_id: int) -> tuple[float, float, float, float]:
         """
-        league_h_goal_avg, league_a_goal_avg = self.get_league_goal_averages()
+        Find and return the attacking and defensive strengths for the given team at 
+        both home and away.
+        
+        Args:
+            team_id (int): The id for the team to evaluate.
+
+        Returns:
+            tuple[float, float, float, float]: The team's
+                - Home attack strength
+                - Home defense strength
+                - Away attack strength
+                - Away defense strength  
+            
+                For the current competition season.
+        """
+        league_h_goal_avg, league_a_goal_avg = self.league_goal_averages()
 
         h_match_filter = select(Match).where(
             and_(
@@ -93,14 +110,22 @@ class Analytics:
 
         return h_atk, h_def, a_atk, a_def
 
-    def get_expected_goals(self, team_ids: HomeAwayID) -> tuple[float, float]:
-        """ 
-        Find and return the expected goal values for the given home and away teams.
+    def expected_goals(self, team_ids: HomeAwayID) -> tuple[float, float]:
         """
-        h_home_atk, h_home_def, _, _ = self.get_team_strengths(team_ids.home_team_id)
-        _, _, a_away_atk, a_away_def = self.get_team_strengths(team_ids.away_team_id)
+        Find and return the expected goal values for the given matchup.
+
+        Args:
+            team_ids (HomeAwayID): The ids for the home and away teams to evaluate.
+
+        Returns:
+            tuple[float, float]: The matchup's:
+                - Home expected goals
+                - Away expected goals
+        """
+        h_home_atk, h_home_def, _, _ = self.team_strengths(team_ids.home_team_id)
+        _, _, a_away_atk, a_away_def = self.team_strengths(team_ids.away_team_id)
         
-        league_h_goal_avg, league_a_goal_avg = self.get_league_goal_averages()
+        league_h_goal_avg, league_a_goal_avg = self.league_goal_averages()
         
         home_xg = (h_home_atk * a_away_def) * league_h_goal_avg
         away_xg = (a_away_atk * h_home_def) * league_a_goal_avg
@@ -109,9 +134,16 @@ class Analytics:
 
     @staticmethod
     def generate_poisson_values(xg: float) -> list[float]:
-        """ 
+        """
         Generate and return a list of possion values from `0` goals to the model's 
         `MAX_GOAL_THRESHOLD` limit based on the given expected goals value.
+
+        Args:
+            xg (float): The expected goals of the team being evaluated.
+
+        Returns:
+            list[float]: The list of probabilities of scoring `0` to 
+                `MAX_GOAL_THRESHOLD` goals.
         """
         pv = []
         
@@ -121,10 +153,19 @@ class Analytics:
         return pv
 
     def poisson_prediction(self, team_ids: HomeAwayID) -> tuple[float, float, float]:
-        """ 
-        Find and return the win, draw, and loss percentages between two teams.
         """
-        h_xg, a_xg = self.get_expected_goals(team_ids)
+        Find and return the probabilities of a win, draw, and loss.
+        
+        Args:
+            team_ids (HomeAwayID): The ids for the teams in the current matchup.
+            
+        Returns:
+            tuple[float, float, float]: The probabilities for a:
+                - Home team win
+                - Draw
+                - Away team win
+        """         
+        h_xg, a_xg = self.expected_goals(team_ids)
         
         h_poisson_values = self.generate_poisson_values(h_xg)
         a_poisson_values = self.generate_poisson_values(a_xg)
@@ -138,22 +179,255 @@ class Analytics:
                 pv_row.append(cell)
             score_matrix.append(pv_row)
         
-        win, loss, draw = 0.0, 0.0, 0.0
+        home_win_p, away_win_p, draw_p = 0.0, 0.0, 0.0
         
         for row in range(len(score_matrix)):
             for col in range (len(score_matrix[row])):
                 if row == col: # draw
-                    draw += score_matrix[row][col]
+                    draw_p += score_matrix[row][col]
                 elif row > col: # win
-                    win += score_matrix[row][col]
+                    home_win_p += score_matrix[row][col]
                 else: # loss
-                    loss += score_matrix[row][col]
+                    away_win_p += score_matrix[row][col]
         
-        total_sum = win + loss + draw
+        total_sum = home_win_p + away_win_p + draw_p
         
-        win /= total_sum
-        draw /= total_sum
-        loss /= total_sum
+        home_win_p /= total_sum
+        draw_p /= total_sum
+        away_win_p /= total_sum
                     
-        return win, loss, draw
+        return home_win_p, draw_p, away_win_p
+    
+  
+    def outcome_percentages(self, team_id: int) -> tuple[float, float, float]:
+        """
+        Find and return the win, loss, and draw rates for a particular team.
+        
+        Args:
+            team_id (int): The id of team to evaluate.
             
+        Returns:
+            tuple[float, float, float]: The rates for a team:
+                - Win
+                - Loss
+                - Draw
+        """
+        wins, losses, draws, total_matches = self.team_outcomes(team_id)
+        
+        if not total_matches:
+            return 0.0, 0.0, 0.0
+                    
+        wins /= total_matches
+        draws /= total_matches
+        losses /= total_matches
+        
+        return wins, draws, losses
+
+    def ppg(self, team_id: int) -> float:
+        """
+        Find and return the points per game (PPG) ratio of the given team.
+
+        Args:
+            team_id (int): The id of the team to evaluate.
+
+        Returns:
+            float: The team's PPG.
+        """
+        wins, _, draws, total_matches = self.team_outcomes(team_id)
+        total_points = (wins * 3) + draws
+        
+        return total_points / total_matches
+
+    def btts_rate(self) -> float:
+        """
+        Find and return the rate of both teams scoring a goal (BTTS) across all 
+        competition season matches. 
+
+        Returns:
+            float: The competition season BTTS rate.
+        """
+        filter = select(Match).where(
+            and_(
+                Match.competition_id == self.competition_id,
+                Match.season_id == self.season_id,
+            )
+        )
+        matches = self.session.exec(filter).all()
+        total_matches = len(matches)
+        
+        if not total_matches:
+            return 0.0
+        
+        bts = 0
+        
+        for match in matches:
+            if match.home_goals > 0 and match.away_goals > 0:
+                bts += 1
+                
+        return bts / total_matches
+      
+    def over_rate(self) -> float:
+        """
+        Find and return the rate of over 2.5 goals being scored across all competition 
+        season matches. 
+
+        Returns:
+            float: The rate of over 2.5 goals being scored.
+        """
+        filter = select(Match).where(
+            and_(
+                Match.competition_id == self.competition_id,
+                Match.season_id == self.season_id
+            )
+        )
+        matches = self.session.exec(filter).all()
+        total_matches = len(matches)
+        
+        if not total_matches:
+            return 0.0
+        
+        over_count = 0
+        
+        for match in matches:
+            if match.home_goals + match.away_goals >= 3:
+                over_count += 1
+        
+        return over_count / total_matches
+        
+    def league_table_stats(
+        self, 
+        team_id: int
+    ) -> tuple[int, int, int, int, int, int, int, int]:
+        """
+        Calculate the given team's wins, losses, draws, points, and goal difference
+        for the league table.
+
+        Args:
+            team_id (int): The id of the team to evaluate.
+
+        Returns:
+            tuple[int, int, int, int, int, int, int, int]: The following table stats:
+                - Wins
+                - Losses
+                - Draws
+                - Matches played
+                - Goals for
+                - Goals against
+                - Goal difference
+                - League points
+
+                For the current competition season.
+        """
+        wins, losses, draws, total_matches = self.team_outcomes(team_id)
+        
+        filter = select(Match).where(
+            and_(
+                Match.competition_id == self.competition_id,
+                Match.season_id == self.season_id,
+                or_(
+                    Match.home_team_id == team_id,
+                    Match.away_team_id == team_id,
+                ),
+            )
+        )
+        team_matches = self.session.exec(filter).all()
+        
+        gf, ga = 0, 0
+        for match in team_matches:
+            if match.home_team_id == team_id:
+                gf += match.home_goals
+                ga += match.away_goals
+            elif match.away_team_id == team_id:
+                gf += match.away_goals
+                ga += match.home_goals
+        
+        gd = gf - ga
+        points = (wins * 3) + draws
+        
+        return wins, losses, draws, total_matches, gf, ga, gd, points
+    
+    def scoreline_chance(self, team_ids: HomeAwayID) -> tuple[int, int]:
+        """
+        Find and return the most likely scoreline to occur in the given matchup.
+
+        Args:
+            team_ids (HomeAwayID): The team ids in the matchup.
+
+        Returns:
+            tuple[int, int]: The most likely scoreline of the format `(0, 0)`, where 
+                index `0` is the home team and index `1` is the away team.
+        """
+        h_xg, a_xg = self.expected_goals(team_ids)
+        
+        h_poisson_values = self.generate_poisson_values(h_xg)
+        a_poisson_values = self.generate_poisson_values(a_xg)
+        
+        score_matrix = []
+        
+        for home_pvs in h_poisson_values:
+            pv_row = []
+            for away_pvs in a_poisson_values:
+                cell = home_pvs * away_pvs
+                pv_row.append(cell)
+            score_matrix.append(pv_row)
+        
+        m = 0
+        scoreline = (0, 0) # home - away
+        for row in range(len(score_matrix)):
+            for col in range(len(score_matrix[row])):
+                temp = max(m, score_matrix[row][col])
+                if temp > m:
+                    m = temp
+                    scoreline = (row, col)
+                
+        return scoreline
+        
+    def team_outcomes(self, team_id: int) -> tuple[int, int, int, int]:
+        """
+        Find and return the number of wins, draws, and total matches played by the
+        given team in the current competition and season.
+
+        Args:
+            team_id (int): The id for the team to evaluate.
+
+        Returns:
+            tuple[int, int, int, int]: The following team stats:
+                - Wins
+                - Losses
+                - Draws
+                - Total competition season matches played
+        """
+        filter = select(Match).where(
+            and_(
+                Match.competition_id == self.competition_id,
+                Match.season_id == self.season_id,
+                or_(
+                    Match.home_team_id == team_id,
+                    Match.away_team_id == team_id,
+                ),
+            )
+        )
+        team_matches = self.session.exec(filter).all()
+        total_matches = len(team_matches)
+        wins, draws, losses = 0, 0, 0
+        
+        if not total_matches:
+            return 0, 0, 0, 0
+        
+        for match in team_matches:
+            if match.home_team_id == team_id:
+                if match.home_goals > match.away_goals:
+                    wins += 1
+                elif match.home_goals < match.away_goals:
+                    losses += 1
+                else:
+                    draws += 1
+            elif match.away_team_id == team_id:
+                if match.away_goals > match.home_goals:
+                    wins += 1
+                elif match.away_goals < match.home_goals:
+                    losses += 1
+                else:
+                    draws += 1
+                    
+        return wins, losses, draws, total_matches
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
-from sqlmodel import and_, SQLModel, create_engine, Session, select, or_
+from sqlmodel import SQLModel, create_engine, Session, select, or_
 
 from analytics import Analytics, HomeAwayID
 from crud import get_team_by_id
@@ -17,9 +17,18 @@ async def setup_db(app: FastAPI):
 
 app = FastAPI(title='Poisson Soccer Prediction Model', lifespan=setup_db)
 
+# +============================+
+#         General Routes
+# +============================+
+
 @app.get('/')
 def home():
     return {'message': 'Soccer Predictor API'}
+
+@app.get('/teams/{team_id}')
+def get_team(team_id: int):
+    with Session(engine) as session:
+        return _get_team_or_404(session, team_id)
 
 @app.get('/teams')
 def get_teams():
@@ -46,65 +55,103 @@ def get_team_competitions():
     with Session(engine) as session:
         return session.exec(select(TeamCompetition)).all()
 
+# +============================+
+#     League Specific Routes
+# +============================+
+
 @app.get('/leagues/{competition_id}/{season_id}/goal_averages')
 def get_league_goal_avg(competition_id: int, season_id: int):
-    curr_data = _create_analytic_data(competition_id, season_id)
-    h_league_goal_avg, a_league_goal_avg = curr_data.get_league_goal_averages()
+    h_league_goal_avg, a_league_goal_avg = _create_analytic_data(
+        competition_id, 
+        season_id
+    ).league_goal_averages()
     
     return {
         'home_league_goal_avg': h_league_goal_avg, 
-        'away_league_goal_avg': a_league_goal_avg
+        'away_league_goal_avg': a_league_goal_avg,
     }
+
+@app.get('/leagues/{competition_id}/{season_id}/btts_rate')
+def get_btts_rate(competition_id: int, season_id: int):
+    btts_rate = _create_analytic_data(competition_id, season_id).btts_rate()
+    
+    return {
+        'both_teams_to_score_rate': btts_rate,
+    }
+
+@app.get('/leagues/{competition_id}/{season_id}/over_rate')
+def get_over_rate(competition_id: int, season_id: int):
+    over_rate = _create_analytic_data(competition_id, season_id).over_rate()
+    
+    return {
+        'over_2.5_goals': over_rate,
+    }
+
+# +============================+
+#     Matchup Specific Routes
+# +============================+
+
+@app.get('/{competition_id}/{season_id}/matchup/expected_goals')
+def get_team_xgs(competition_id: int, season_id: int, home_id: int, away_id: int):
+    h_xg, a_xg = _create_analytic_data(
+        competition_id, 
+        season_id
+    ).expected_goals(_create_home_and_away_data(home_id, away_id))
+    
+    return {
+        'home_expected_goals': h_xg,
+        'away_expected_goals': a_xg,
+    }
+
+@app.get('/{competition_id}/{season_id}/matchup/prediction')
+def get_prediction(competition_id: int, season_id: int, home_id: int, away_id: int):
+    home_win, away_win, draw = _create_analytic_data(
+        competition_id, 
+        season_id,
+    ).poisson_prediction(_create_home_and_away_data(home_id, away_id))
+    
+    return {
+        'home_win_probability': round((home_win * 100), 3),
+        'away_win_probability': round((away_win * 100), 3),
+        'draw_probability': round((draw * 100), 3),
+    }
+
+@app.get('/{competition_id}/{season_id}/matchup/scoreline_likelihood')
+def get_scoreline_likelihood(
+    competition_id: int, 
+    season_id: int, 
+    home_id: int, 
+    away_id: int,
+):
+    scoreline = _create_analytic_data(
+        competition_id, 
+        season_id,
+    ).scoreline_chance(_create_home_and_away_data(home_id, away_id))
+    
+    return {
+        'most_likely_scoreline': scoreline,
+    }
+
+# +============================+
+#     Team Specific Routes
+# +============================+
 
 @app.get('/teams/{team_id}/{competition_id}/{season_id}/strengths')
 def get_team_strengths(competition_id: int, season_id: int, team_id: int):
-    curr_data = _create_analytic_data(competition_id, season_id)
-    h_atk, h_def, a_atk, a_def = curr_data.get_team_strengths(team_id)
+    h_atk, h_def, a_atk, a_def = _create_analytic_data(
+        competition_id, 
+        season_id
+    ).team_strengths(team_id)
 
     return {
         'home_attack': h_atk, 
         'home_defense': h_def, 
         'away_attack': a_atk, 
-        'away_defense': a_def
+        'away_defense': a_def,
     }
 
-def get_match_expected_goals(competition_id: int, season_id: int, home_id, away_id):
-    with Session(engine) as session:
-        curr_teams = HomeAwayID(home_id, away_id)
-        curr_data = _create_analytic_data(competition_id, season_id)
-        filter = select(Match).where(
-            and_(Match.competition_id == competition_id, Match.season_id == season_id)
-        )
-        matchups = session.exec(filter).all()
-
-
-def _create_analytic_data(competition_id: int, season_id: int) -> Analytics:
-    with Session(engine) as session:
-        return Analytics(session, competition_id, season_id)
-
-def _create_home_and_away_data(home_team_id: int, away_team_id: int) -> HomeAwayID:
-    return HomeAwayID(home_team_id, away_team_id)
-
-# @app.get('/teams/{team_id}/league/average_goals')
-# def get_league_average_goals(team_id):
-#     with Session(engine) as session:
-#         team = _get_team_or_404(session, team_id)
-#         # league = team.league
-#         h_lga, a_lga = get_league_goal_averages(session)
-#         return {'home_league_goal_avg': h_lga, 'away_league_goal_avg': a_lga}
-
-# @app.get('/teams/{analytics}/competition/average_goals')
-# def get_league_average_goals():
-#     with Session(engine) as session:
-#         analytic = Analytics(session, )
-
-@app.get('/teams/{team_id}')
-def get_team(team_id: int):
-    with Session(engine) as session:
-        return _get_team_or_404(session, team_id)
-
 @app.get('/teams/{team_id}/goals')
-def get_scores(team_id: int):
+def get_team_goals(team_id: int):
     with Session(engine) as session:        
         filter = select(Match).where(
             or_(Match.home_team_id == team_id, Match.away_team_id == team_id)
@@ -120,8 +167,58 @@ def get_scores(team_id: int):
         
         return {
             'team': _get_team_or_404(session, team_id).name, 
-            'total_goals_scored': goals
+            'total_goals_scored': goals,
         }
+
+@app.get('/teams/{team_id}/{competition_id}/{season_id}/outcome_rates')
+def get_outcome_rates(competition_id: int, season_id: int, team_id: int):
+    wins, draws, losses = _create_analytic_data(
+        competition_id, 
+        season_id
+    ).outcome_percentages(team_id)
+    
+    return {
+        'win_rate': wins,
+        'loss_rate': losses,
+        'draw_rate': draws,
+    }
+
+@app.get('/teams/{team_id}/{competition_id}/{season_id}/ppg')
+def get_team_ppg(competition_id: int, season_id: int, team_id: int):
+    ppg = _create_analytic_data(competition_id, season_id).ppg(team_id)
+    
+    return {
+        'points_per_game': ppg
+    }
+
+@app.get('/teams/{team_id}/{competition_id}/{season_id}/league_table_data')
+def get_team_league_table_stats(competition_id: int, season_id: int, team_id: int):
+    wins, losses, draws, total_matches, gf, ga, gd, points, = _create_analytic_data(
+        competition_id, 
+        season_id
+    ).league_table_stats(team_id)
+    
+    return {
+        'wins': wins,
+        'losses': losses,
+        'draws': draws,
+        'matches_played': total_matches,
+        'goals_for': gf,
+        'goals_against': ga,
+        'goal_difference': gd,
+        'points': points,
+    }
+   
+# +============================+
+#       Helper Functions
+# +============================+
+    
+def _create_analytic_data(competition_id: int, season_id: int) -> Analytics:
+    with Session(engine) as session:
+        return Analytics(session, competition_id, season_id)
+
+def _create_home_and_away_data(home_team_id: int, away_team_id: int) -> HomeAwayID:
+    return HomeAwayID(home_team_id, away_team_id)
 
 def _get_team_or_404(session: Session, team_id: int) -> Team:
     team = get_team_by_id(session, team_id)
@@ -130,37 +227,3 @@ def _get_team_or_404(session: Session, team_id: int) -> Team:
         raise HTTPException(status_code=404, detail='Team not found')
     
     return team
-
-# # debug
-# @app.get('/test/strengths/{team_id}')
-# def test_strengths(team_id: int):
-#     with Session(engine) as session:
-#         avg_home_goals, avg_away_goals = get_league_goal_averages(session)
-        
-#         h_atk, h_def, a_atk, a_def = get_team_strengths(session, team_id)
-        
-#         return {
-#             'team_id': team_id,
-#             'baseline': {
-#                 'avg_league_home_goals': avg_home_goals,
-#                 'avg_league_away_goals': avg_away_goals,
-#             },
-#             'strengths': {
-#                 'home_attack': h_atk,
-#                 'home_defense': h_def,
-#                 'away_attack': a_atk,
-#                 'away_defense': a_def,
-#             }
-#         }
-
-# # debug
-# def main():
-#     with Session(engine) as session:
-#         win, loss, draw = poisson_prediction(session, 1, 2)
-#         print(f'{win:%}')
-#         print(f'{draw:%}')
-#         print(f'{loss:%}')
-
-        
-# if __name__ == '__main__':
-#     main()
